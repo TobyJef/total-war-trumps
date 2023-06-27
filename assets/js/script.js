b171c794ce21516dd92e9fee20eb7bffdb778293
TobyJef <>
2023-06-27 21:05:50 +0000

    Add base function names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log("this works")
+// Wait to start game after DOM has finished loading
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+})
+
+
+function runGame() {}
+
+function checkResult(){}
+
+function displayVictory(){}
