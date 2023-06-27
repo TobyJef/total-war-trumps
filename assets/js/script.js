989741bad0b611c278b0fd8621db0fa388699c87
TobyJef <>
2023-06-27 21:35:42 +0000

    Add victory decision function

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,59 @@ function runGame() {}
 function checkResult(){}
 
 function displayVictory(){}
+
+// Random number generator to assign to cpu choice
+
+function generateCpuChoice () {
+    const randomNumber = Math.floor(Math.random() *4) + 1
+
+    if (randomNumber === 1) {
+        cpuChoice = 'archer' 
+    }
+
+    if (randomNumber === 2){
+        cpuChoice = 'samurai'
+    }
+
+    if (randomNumber === 3){
+        cpuChoice = 'spearmen'
+    }
+
+    if (randomNumber === 4){
+        cpuChoice = 'cavalry'
+    }
+}
+
+function decideVictory(){
+    if (cpuChoice === playerChoice){
+        result = 'Draw'
+    }
+
+    if (cpuChoice === 'archer' && playerChoice === 'samurai'){
+        result = 'Shameful Defeat'
+    }
+
+     if (cpuChoice === 'samurai' && playerChoice === 'spearmen'){
+        result = 'Shameful Defeat'
+    }
+
+     if (cpuChoice === 'spearmen' && playerChoice === 'cavalry'){
+        result = 'Shameful Defeat'
+    }
+
+     if (cpuChoice === 'cavalry' && playerChoice === 'archer'){
+        result = 'Shameful Defeat'
+    }
+     if (cpuChoice === 'samurai' && playerChoice === 'archer'){
+        result = 'Glorious Victory!'
+    }
+     if (cpuChoice === 'spearmen' && playerChoice === 'samurai'){
+        result = 'Glorious Victory!'
+    }
+     if (cpuChoice === 'cavalry' && playerChoice === 'spearmen'){
+        result = 'Glorious Victory!'
+    }
+     if (cpuChoice === 'archer' && playerChoice === 'cavalry'){
+        result = 'Glorious Victory!'
+    }
+}
