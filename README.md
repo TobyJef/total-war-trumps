f6821116806ecd3cac111bb583bd5c77d52e5b7a
TobyJef <>
2023-06-28 23:07:07 +0000

    add start of new function

--- README.md
+++ README.md
@@ -5,40 +5,38 @@ Total War Trumps is a game based on the classic game of Rock, Paper, Scissors. B
 Similar in logic to Rock, Paper, Scissors where Rock beats Scissors, Scissors beats Paper and Paper beats Rock. Total War Trumps works on the same logic of superiority. In this game, the units consist of Archers, Samurai, Yari Spearmen and Cavalry.
 Archers trump Samurai, Samurai trump Yari Spearmen, Yari Spearmean trump Cavalry and finally Cavalry trumps Archers.
 
-*Live site link to go here*
+_Live site link to go here_
 
 Features
 
-
-
 Technologies Used
+
 - HTML
 - CSS
 - JavaScript
 
-Content 
+Content
 
 Images
+
 - Game unit images taken by screenshot of my personal copy of Total War: Shogun 2
-- https://www.favicon-generator.org/ for Favicon image 
+- https://www.favicon-generator.org/ for Favicon image
 - Crossed sword image found via Bing image search
 - Logo image found and saved from Shutterstock
-
 
 Future Improvements
 
 - For a future update it would be my goal to add a more units, adding complexity to the game.
-- I would also like to include a drop down option, allowing players to change the time period of the in game units   
+- I would also like to include a drop down option, allowing players to change the time period of the in game units  
   to further expand the roster. i.e. A Roman Empire update, Roman Urban Cohorts taking place of Sword Samurai.
-
 
 Testing
 
-  Deployment
+Deployment
 
-  - The site Total War: Trumps was deployed to Github pages.
+- The site Total War: Trumps was deployed to Github pages.
 
-To deploy the page on Github pages, the steps below are as follows: 
+To deploy the page on Github pages, the steps below are as follows:
 
 Login to Github.
 Go to the repository for this project, TobyJef/total-war-trumps.
@@ -46,7 +44,7 @@ Click the settings button.
 Select pages in the left hand navigation menu.
 From the source dropdown, select main branch and press save.
 Following a short delay, the deployment will have been completed and the page provides a link to the completed website
-The live link can be found here *insert live link*
+The live link can be found here _insert live link_
 
 Bugs
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,93 +1,123 @@
 // Wait to start game after DOM has finished loading
 // code 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
-})
+});
 
-const playerChoice = document.getElementByID('player-choice')
-const cpuChoice = document.getElementByID('cpu-choice')
-const resultDisplay = document.getElementsByClassName('score')
+const selectionButtons = document.querySelectorAll('[data-selction');
 
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection;
+        makeSelection(selectName);
+    });
+});
 
-function runGame() {}
+function makeSelection(selection) {
+    console.log(selection);
+}
+
+const playerChoice = document.getElementByID('player-choice');
+const cpuChoice = document.getElementByID('cpu-choice');
+const resultDisplay = document.getElementsByClassName('score');
+const possibleChoices = document.getElementsByClassName('selection');
+const playerChoiceDisplay;
+const cpuChoiceDisplay;
+getResult;
+
+function runGame() { }
 
 // Random number generator used to generate cpu choice
 
-function generateCpuChoice () {
-    const randomNumber = Math.floor(Math.random() *4) + 1
+function generateCpuChoice() {
+    const randomNumber = Math.floor(Math.random() * 4) + 1;
 
     if (randomNumber === 1) {
-        cpuChoice = 'archer' 
+        cpuChoice = 'archer';
     }
 
-    if (randomNumber === 2){
-        cpuChoice = 'samurai'
+    if (randomNumber === 2) {
+        cpuChoice = 'samurai';
     }
 
-    if (randomNumber === 3){
-        cpuChoice = 'spearmen'
+    if (randomNumber === 3) {
+        cpuChoice = 'spearmen';
     }
 
-    if (randomNumber === 4){
-        cpuChoice = 'cavalry'
+    if (randomNumber === 4) {
+        cpuChoice = 'cavalry';
     }
+
+    cpuChoiceDisplay.innerHTML = cpuChoice;
 }
 
 // Victory conditions
 
-function decideVictory(){
-    if (cpuChoice === playerChoice){
-        result = 'Draw'
+function decideVictory() {
+    if (cpuChoice === playerChoice) {
+        result = 'Draw';
     }
 
-    if (cpuChoice === 'archer' && playerChoice === 'samurai'){
-        result = 'Shameful Defeat'
-        console.log (loss);
+    if (cpuChoice === 'archer' && playerChoice === 'samurai') {
+        result = 'Shameful Defeat';
+        console.log(loss);
     }
 
-     if (cpuChoice === 'samurai' && playerChoice === 'spearmen'){
-        result = 'Shameful Defeat'
-        console.log (loss);
+    if (cpuChoice === 'samurai' && playerChoice === 'spearmen') {
+        result = 'Shameful Defeat';
+        console.log(loss);
     }
 
-     if (cpuChoice === 'spearmen' && playerChoice === 'cavalry'){
-        result = 'Shameful Defeat'
-        console.log (loss);
+    if (cpuChoice === 'spearmen' && playerChoice === 'cavalry') {
+        result = 'Shameful Defeat';
+        console.log(loss);
     }
 
-     if (cpuChoice === 'cavalry' && playerChoice === 'archer'){
-        result = 'Shameful Defeat'
-        console.log (loss);
+    if (cpuChoice === 'cavalry' && playerChoice === 'archer') {
+        result = 'Shameful Defeat';
+        console.log(loss);
     }
-     if (cpuChoice === 'samurai' && playerChoice === 'archer'){
-        result = 'Glorious Victory!'
-        console.log (win);
+    if (cpuChoice === 'samurai' && playerChoice === 'archer') {
+        result = 'Glorious Victory!';
+        console.log(win);
     }
-     if (cpuChoice === 'spearmen' && playerChoice === 'samurai'){
-        result = 'Glorious Victory!'
-        console.log (win);
+    if (cpuChoice === 'spearmen' && playerChoice === 'samurai') {
+        result = 'Glorious Victory!';
+        console.log(win);
     }
-     if (cpuChoice === 'cavalry' && playerChoice === 'spearmen'){
-        result = 'Glorious Victory!'
-        console.log (win);
+    if (cpuChoice === 'cavalry' && playerChoice === 'spearmen') {
+        result = 'Glorious Victory!';
+        console.log(win);
     }
-     if (cpuChoice === 'archer' && playerChoice === 'cavalry'){
-        result = 'Glorious Victory!'
-        console.log (win);
+    if (cpuChoice === 'archer' && playerChoice === 'cavalry') {
+        result = 'Glorious Victory!';
+        console.log(win);
     }
 
-    resultDisplay.innerHTML = result
+    resultDisplay.innerHTML = result;
 }
 
-// Display Outcome function - best out of 3 or 5 vs cpu
+// Display Outcome function - best out of 5 vs cpu
 
-function displayOutcome(){
+function displayOutcome() {
 
-    if win > loss Congratulations, you have defended your Kingdom!
-    if win < loss You have lost control of your Kingdom
+    let win = 3;
+    let result;
+
+    if (win >= 3) {
+        result = "Congratulations, you have defended your Kingdom!";
+    } else if (win < 3) {
+        result = "You have lost control of your Kingdom";
+    }
+
+
+    if win > loss Congratulations, you have defended your Kingdom!;
+    if win < loss You have lost control of your Kingdom;
 
     let outcome = decideVictory();
-    
+
 
 }
+
+// 
